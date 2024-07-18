bb64d18de6766fc3a8295e67626f8c4d8facb54a
Dave McCollough <davemccollough@gmail.com>
2024-07-17 20:38:16 -0600

    Initial Commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func addClick(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if  let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
 
-
+    @IBAction func minusClick(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if  let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func multiplyClick(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if  let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func divideClick(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!){
+            if  let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
 }
 
